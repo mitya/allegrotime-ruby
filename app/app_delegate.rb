10d422c2f59dc75d5636dc0bd575a18a72a85865
Dmitry Sokurenko <dmitry@sokurenko.name>
2014-04-12 01:31:19 +0400

    wip

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,97 +5,91 @@ class AppDelegate
     model = ModelManager.alloc.init
     app = self
     
-    mainViewController = MainViewController.alloc.initWithNibName("MainView", bundle:nil)
-    self.navigationController = UINavigationController.alloc.initWithRootViewController(mainViewController)
-    self.navigationController.delegate = self
+    mainViewController = MainViewController.alloc.initWithNibName "MainView", bundle:nil
+    self.navigationController = UINavigationController.alloc.initWithRootViewController mainViewController
+    navigationController.delegate = self
 
-    self.window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    self.window.backgroundColor = UIColor.whiteColor
-    self.window.rootViewController = self.navigationController
-    self.window.makeKeyAndVisible
+    self.window = UIWindow.alloc.initWithFrame UIScreen.mainScreen.bounds
+    window.backgroundColor = UIColor.whiteColor
+    window.rootViewController = self.navigationController
+    window.makeKeyAndVisible
 
-    self.perMinuteTimer = NSTimer.scheduledTimerWithTimeInterval(20, target:self, selector:'timerTicked', userInfo:nil repeats:YES)
-    self.perMinuteTimer.fireDate = Helper.nextFullMinuteDate
+    self.perMinuteTimer = NSTimer.scheduledTimerWithTimeInterval 20, target:self, selector:'timerTicked', userInfo:nil repeats:YES
+    perMinuteTimer.fireDate = Helper.nextFullMinuteDate
 
     true
   end
 
   def applicationDidBecomeActive(application)
-    self.locationManager.startUpdatingLocation if CLLocationManager.locationServicesEnabled
+    locationManager.startUpdatingLocation if CLLocationManager.locationServicesEnabled
   end
 
-  # - (void)applicationWillResignActive:(UIApplication *)application {
-  #   [self.locationManager stopUpdatingLocation];
-  # }
-  # 
-  # - (void)applicationWillEnterForeground:(UIApplication *)application {
-  #   [self triggerModelUpdateFor:self.navigationController.visibleViewController];
-  # }
-  # 
-  # - (void)applicationDidEnterBackground:(UIApplication *)application {
-  # }
-  # 
-  # - (void)applicationWillTerminate:(UIApplication *)application {
-  # }
-  # 
-  # 
-  # #pragma mark - Location Tracking
-  # 
-  # - (CLLocationManager *)locationManager {
-  #   if (!locationManager) {
-  #     locationManager = [CLLocationManager new];
-  #     locationManager.delegate = self;
-  #     locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
-  #     locationManager.distanceFilter = 100;
-  #   }
-  #   return locationManager;
-  # }
-  # 
-  # - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-  #   MXWriteToConsole(@"didUpdateToLocation acc=%.f dist=%.f %@", newLocation.horizontalAccuracy, [newLocation distanceFromLocation:oldLocation], model.closestCrossing.name);
-  # 
-  #   Crossing *const newClosestCrossing = [model crossingClosestTo:newLocation];
-  #   if (newClosestCrossing != model.closestCrossing) {
-  #     model.closestCrossing = newClosestCrossing;
-  #     [[NSNotificationCenter defaultCenter] postNotificationName:NXClosestCrossingChanged object:model.closestCrossing];
-  #   }
-  # }
-  # 
-  # - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-  #   MXWriteToConsole(@"locationManager:didFailWithError: %@", error);
-  # 
-  #   model.closestCrossing = nil;
-  #   [[NSNotificationCenter defaultCenter] postNotificationName:NXClosestCrossingChanged object:model.closestCrossing];
-  # }
-  # 
-  # #pragma mark - handlers
-  # 
-  # - (void)navigationController:(UINavigationController *)navController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-  #   [self.navigationController setToolbarHidden:(viewController.toolbarItems.count == 0) animated:animated];
-  # 
-  #   if (animated)
-  #     [self triggerModelUpdateFor:viewController];
-  # }
-  # 
-  # - (void)timerTicked {
-  #   [self triggerModelUpdateFor:self.navigationController.visibleViewController];
-  # }
-  # 
-  # - (void)triggerModelUpdateFor:(UIViewController *)controller {
-  #   if ([controller respondsToSelector:@selector(modelUpdated)]) {
-  #     [controller performSelector:@selector(modelUpdated)];
-  #   }
-  # }
-  # 
-  # #pragma mark - properties
-  # 
-  # - (CrossingMapController *)mapController {
-  #   if (!mapController)
-  #     mapController = [[CrossingMapController alloc] init];
-  #   return mapController;
-  # }
-  # 
-  # @end
+  def applicationWillResignActive(application)
+    locationManager.stopUpdatingLocation
+  end
 
+  def applicationWillEnterForeground(application)
+    triggerModelUpdateFor navigationController.visibleViewController
+  end
+  
+  def applicationDidEnterBackground(application)
+  end
+  
+  def applicationWillTerminate(application)
+  end
+    
+  ### Location Tracking
+  
+  def locationManager
+    unless @locationManager
+      @locationManager = CLLocationManager.new
+      @locationManager.delegate = self
+      @locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+      @locationManager.distanceFilter = 100
+    end
+    @locationManager
+  end
+
+  def locationManager(manager, didUpdateToLocation:newLocation, fromLocation:oldLocation)
+    MXWriteToConsole(
+      "didUpdateToLocation acc=%.f dist=%.f %@", 
+      newLocation.horizontalAccuracy, newLocation.distanceFromLocation(oldLocation), model.closestCrossing.name)
+  
+    newClosestCrossing = model.crossingClosestTo(newLocation)
+    if newClosestCrossing != model.closestCrossing
+      model.closestCrossing = newClosestCrossing
+      NSNotificationCenter.defaultCenter.postNotificationName NXClosestCrossingChanged, object:model.closestCrossing
+    end
+  end
+
+  def locationManager(manager, didFailWithError:error)
+    MXWriteToConsole("locationManager:didFailWithError: %@", error)
+  
+    model.closestCrossing = nil
+    NSNotificationCenter.defaultCenter.postNotificationName NXClosestCrossingChanged, object:model.closestCrossing
+  end
+  
+  ### handlers
+  
+  def navigationController(navController, willShowViewController:viewController, animated:animated)
+    navigationController.setToolbarHidden viewController.toolbarItems.count == 0, animated:animated
+    triggerModelUpdateFor.viewController if (animated)
+  end
+  
+  def timerTicked
+    triggerModelUpdateFor navigationController.visibleViewController
+  end
+  
+  def triggerModelUpdateFor(controller)
+    if controller.respondsToSelector 'modelUpdated'
+      controller.performSelector 'modelUpdated'
+    end
+  end
+  
+  ### properties
+  
+  def mapController
+    @mapController ||= CrossingMapController.alloc.init
+  end
 end
 
